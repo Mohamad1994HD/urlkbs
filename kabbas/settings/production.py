1cb9aacc58ab8e973169f3eaae0546ab3c71807c
HD <mohd1haddad@gmail.com>
2017-03-20 16:47:09 +0300

    ADMINS added to debug

--- kabbas/settings/production.py
+++ kabbas/settings/production.py
@@ -14,6 +14,8 @@ SECRET_KEY = 'y7eesetu_%n+j$5zdz&991gcoucztk&$&koazbhjdl^4w0u2x&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ADMINS = [('Mohd','mohd1haddad@gmail.com'),]
+
 ALLOWED_HOSTS = ["www.urlkbs.life", "urlkbs.life", "api.urlkbs.life", "kabbas.herokuapp.com"]
 
 
