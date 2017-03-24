054fd95b12e0a51fee9a2a3a6ea4283a73c0d586
HD <mohd1haddad@gmail.com>
2017-03-24 18:36:01 +0300

    Modified api url namespace

--- kabbas/urls.py
+++ kabbas/urls.py
@@ -20,6 +20,6 @@ from shortener.views import HomeView, RedirectURLView, ApiView
 urlpatterns = [
    url(r'^$', HomeView.as_view(), name='home'),
    url(r'^admin/', admin.site.urls),
-   url(r'^api/', ApiView.as_view(), name='api'),
+   url(r'^api/', ApiView.as_view(), name='api-doc'),
    url(r'^(?P<code>[\w-]+)/$', RedirectURLView.as_view(), name='short'),
-        ]
+]        
